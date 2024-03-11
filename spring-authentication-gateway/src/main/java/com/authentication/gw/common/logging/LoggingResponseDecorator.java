a293ab033c662cbe72f5378221fc1ae0de3e89c5
seolminsu90 <seolminsu90@naver.com>
2024-03-11 14:25:21 +0900

    update response logging

--- spring-authentication-gateway/src/main/java/com/authentication/gw/common/logging/LoggingResponseDecorator.java
+++ spring-authentication-gateway/src/main/java/com/authentication/gw/common/logging/LoggingResponseDecorator.java
@@ -1,15 +1,22 @@
 package com.authentication.gw.common.logging;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.jetbrains.annotations.NotNull;
+import org.reactivestreams.Publisher;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 public class LoggingResponseDecorator extends ServerHttpResponseDecorator {
 
     public LoggingResponseDecorator(ServerHttpResponse response) {
         super(response);
-        log.info("Status {} ", response.getStatusCode());
+    }
+
+    @Override
+    public @NotNull Mono<Void> writeWith(@NotNull Publisher<? extends DataBuffer> body) {
+        return super.writeWith(body).doOnTerminate(() -> log.info("Status {} ", getStatusCode()));
     }
 }
